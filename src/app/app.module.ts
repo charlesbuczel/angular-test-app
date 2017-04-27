ed55aa4cc7f4af1ea2200c650ccf741de694f988
Charles Buczel <charles.buczel@gmail.com>
2017-04-27 23:27:41 +0100

    Add test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HiComponent } from './hi/hi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HiComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +17,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HiComponent]
 })
 export class AppModule { }
